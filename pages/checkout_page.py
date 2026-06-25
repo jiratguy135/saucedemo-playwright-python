f24ef3c23c1753114a3054e4762dd9d81f21a33d
Guyji <jirat.fuangfu@gmail.com>
2026-06-25 21:32:14 +0700

    fix: locator and method

--- pages/checkout_page.py
+++ pages/checkout_page.py
@@ -7,14 +7,33 @@ class CheckoutPage:
         self.continue_button = page.locator('[data-test="continue"]')
         self.finish_button = page.locator('[data-test="finish"]')
 
+        self.firstname_field = page.locator('[data-test="firstName"]')
+        self.lastname_field = page.locator('[data-test="lastName"]')
+        self.postalcode_field = page.locator('[data-test="postalCode"]')
+        self.error_message = page.locator('[data-test="error"]')
+        self.cancel_button = page.locator('[data-test="cancel"]')
+
+        self.complete_header = page.locator('[data-test="complete-header"]')
+        self.complete_text = page.locator('[data-test="complete-text"]')
+        self.back_home_button = page.locator('[data-test="back-to-products"]')
 
 
     def go_to_checkout(self):
         self.checkout_button.click()
 
     def go_to_continue_shopping(self):
-        self.continue_shopping.click()
+        self.continue_shopping_button.click()
 
+    def click_continue(self):
+        self.continue_button.click()
+
+    def click_finish(self):
+        self.finish_button.click()
+
+    def information(self,firstname,lastname,postalcode):
+        self.firstname_field.fill(firstname)
+        self.lastname_field.fill(lastname)
+        self.postalcode_field.fill(postalcode)
     
 
 
--- pages/inventory_page.py
+++ pages/inventory_page.py
@@ -6,6 +6,8 @@ class InventoryPage:
         self.add_backpack_to_cart_button = page.locator('[data-test="add-to-cart-sauce-labs-backpack"]')
         self.remove_backpack_from_cart_button = page.locator('[data-test="remove-sauce-labs-backpack"]')
         self.cart_button = page.locator('[data-test="shopping-cart-link"]')
+        self.cart_badge = page.locator('[data-test="shopping-cart-badge"]')
+
 
     def add_backpack_to_cart(self):
         self.add_backpack_to_cart_button.click()
@@ -15,3 +17,4 @@ class InventoryPage:
 
     def go_to_cart(self):
         self.cart_button.click()
+
